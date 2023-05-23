6348e82e4d787982b7bfdd2341d12ffd18cb5642
Connor Carciofini <ccarciofini@gmail.com>
2023-05-23 16:56:19 -0500

    client js bug

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -4,28 +4,22 @@ const data_node = document.getElementById('data');
 const tokens_list = data_node.children[0].children[1];
 const lemmas_list = data_node.children[1].children[1];
 
-// function createAndAppendListItems(text, list_element){
-//     const list_item = document.createElement('li');
-//     list_item.innerText = text;
-//     list_element.appendChild(list_item);
-// };
-
 function createAndAppendListItems(strings_array, list_element){
     strings_array.map((string) => {
         const list_item = document.createElement('li');
         list_item.innerText = string;
         list_element.appendChild(list_item);
-    })
-}
+    });
+};
 
 function renderData(data){
     tokens_list.innerHTML = '';
-
+    lemmas_list.innerHTML = '';
     createAndAppendListItems(data.tokens, tokens_list);
     createAndAppendListItems(data.lemmas, lemmas_list);
 };
 
-async function getAPIResponse(string){
+async function fetchAPIResponse(string){
     const response = await fetch('/api/', {
         method: 'POST',
         headers: {
@@ -38,7 +32,7 @@ async function getAPIResponse(string){
 
 async function handleSubmit(e){
     e.preventDefault();
-    const response = await getAPIResponse(input.value);
+    const response = await fetchAPIResponse(input.value);
     const data = await response.json();
     renderData(data);
     input.value = '';
