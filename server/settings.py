b0754e93bea55c55b8137ea74d7c3011aa0c707d
Connor Carciofini <ccarciofini@gmail.com>
2023-05-23 22:10:12 -0500

    debug

--- server/settings.py
+++ server/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 IS_HEROKU = 'DYNO' in os.environ
 if IS_HEROKU:
-    DEBUG = False
+    DEBUG = True
     ALLOWED_HOSTS = ['*']
 
 if not IS_HEROKU:
